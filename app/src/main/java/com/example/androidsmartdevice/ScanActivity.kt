0ff36794a5874a7906727626db2ed4883a725e62
georgiaclemencon <georgia.clemencon@isen.yncrea.fr>
2024-04-13 16:44:00 +0200

    Code propre

--- app/src/main/java/com/example/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/com/example/androidsmartdevice/ScanActivity.kt
@@ -1,35 +1,23 @@
 // ScanActivity.kt
 package com.example.androidsmartdevice
 
+import DisplayBluetoothStatus
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class ScanActivity : ComponentActivity() {
@@ -51,175 +39,77 @@ class ScanActivity : ComponentActivity() {
             val isBluetoothEnabled = checkBluetoothStatus(this, requestMultiplePermissionsLauncher)
             if (isBluetoothEnabled) {
                 // Initialiser la liste des appareils et les interactions avec le bouton si le Bluetooth est activé
-                ScanActivityContent()
 
             }
         }
     }
 
 
-}
-// Composable function to display the content of the Scan Activity
-
-@Composable
-fun ScanActivityContent() {
-    val isSquareIcon = remember { mutableStateOf(false) }
-    val isScanning = remember { mutableStateOf(false) }
-    ScanActivityUI(isSquareIcon, isScanning, ::toggleButtonPlayScan)
-}
-
-// Function to toggle the play/scan button
-private fun toggleButtonPlayScan(
-    isSquareIcon: MutableState<Boolean>,
-    isScanning: MutableState<Boolean>
-) {
-    isSquareIcon.value = !isSquareIcon.value
-    isScanning.value = !isScanning.value
-}
-
-
-
-// Function to check if all the permissions are granted
-fun areAllPermissionsGranted(context: Context): Boolean {
-    val permissions = arrayOf(
-        Manifest.permission.BLUETOOTH,
-        Manifest.permission.BLUETOOTH_ADMIN,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
-    return permissions.all {
-        ContextCompat.checkSelfPermission(
-            context,
-            it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-}
-
-@Composable
-fun checkBluetoothStatus(
-    context: Context, requestMultiplePermissionsLauncher: ActivityResultLauncher<Array<String>>,
-): Boolean {
-    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-    if (bluetoothAdapter == null) {
-        // Device doesn't support Bluetooth
-        Toast.makeText(context, "Votre appareil ne supporte pas le Bluetooth", Toast.LENGTH_SHORT)
-            .show()
-        BluetoothNotSupportedScreen()
-        return false
-    } else {
-        if (!bluetoothAdapter.isEnabled) {
-            // Bluetooth is not enabled
-            Toast.makeText(context, "Bluetooth désactivé", Toast.LENGTH_SHORT).show()
-            BluetoothDisabledScreen()
-            // Check if the permission is granted
-            if (ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.BLUETOOTH
-                ) == PackageManager.PERMISSION_GRANTED
-            ) {
-                // Permission is granted, open Bluetooth settings
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                context.startActivity(enableBtIntent)
-            } else {
-                // Permission is not granted, request the permission
-                requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.BLUETOOTH))
-            }
-            return false
-        } else {
-            // Bluetooth is enabled
-            Toast.makeText(context, "Bluetooth activé", Toast.LENGTH_SHORT).show()
-            // Check if all permissions are granted
-            val allPermissionsGranted = areAllPermissionsGranted(context)
-            if (!allPermissionsGranted) {
-                // Not all permissions are granted, request the permissions
-                requestMultiplePermissionsLauncher.launch(arrayOf(
+    @Composable
+    private fun checkAndRequestPermissions(
+        context: Context,
+        requestMultiplePermissionsLauncher: ActivityResultLauncher<Array<String>>
+    ): Boolean {
+        val allPermissionsGranted = areAllPermissionsGranted(context)
+        if (!allPermissionsGranted) {
+            requestMultiplePermissionsLauncher.launch(
+                arrayOf(
                     Manifest.permission.BLUETOOTH,
                     Manifest.permission.BLUETOOTH_ADMIN,
                     Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION
-                ))
-            }
-            return allPermissionsGranted
+                )
+            )
         }
+        return allPermissionsGranted
     }
-}
 
-// Display the list of connected devices
-@Composable
-fun DisplayDevices(isScanning: MutableState<Boolean>, devices: List<String>) {
-    if (isScanning.value) {
-        devices.forEach { device ->
-            Text(text = device, modifier = Modifier.padding(16.dp))
-        }
-    }
-}
+    @Composable
+    private fun checkBluetoothStatus(
+        context: Context,
+        requestMultiplePermissionsLauncher: ActivityResultLauncher<Array<String>>
+    ): Boolean {
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
-// UI of the Scan Activity
-@Composable
-fun ScanActivityUI(
-    isSquareIcon: MutableState<Boolean>,
-    isScanning: MutableState<Boolean>,
-    onIconClick: (MutableState<Boolean>, MutableState<Boolean>) -> Unit
-) {
-    val devices =
-        listOf("Device 1", "Device 2", "Device 3") // This is the fictive list of connected devices
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Scan Activity", modifier = Modifier.padding(16.dp))
-        Image(
-            painter = painterResource(
-                id = if (isSquareIcon.value) R.drawable.baseline_square_24 else R.drawable.baseline_play_arrow_24
-            ),
-            contentDescription = "Scan Icon",
-            modifier = Modifier
-                .size(150.dp)
-                .padding(16.dp)
-                .clickable {
-                    onIconClick(isSquareIcon, isScanning)
+        if (bluetoothAdapter == null) {
+            DisplayBluetoothStatus(context, "notSupported")
+            return false
+        } else {
+            if (!bluetoothAdapter.isEnabled) {
+                DisplayBluetoothStatus(context, "disabled")
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.BLUETOOTH
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    context.startActivity(enableBtIntent)
+                } else {
+                    requestMultiplePermissionsLauncher.launch(arrayOf(Manifest.permission.BLUETOOTH))
                 }
-        )
-        Text(text = "Lancer le scan", modifier = Modifier.padding(16.dp))
-        if (isScanning.value) {
-            LinearProgressIndicator(modifier = Modifier.padding(16.dp))
+
+                return false
+            } else {
+                DisplayBluetoothStatus(context, "enabled")
+                return checkAndRequestPermissions(context, requestMultiplePermissionsLauncher)
+            }
         }
-        DisplayDevices(isScanning, devices) // Pass the devices list here
+    }
+
+
+    private fun areAllPermissionsGranted(context: Context): Boolean {
+        val permissions = arrayOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return permissions.all {
+            ContextCompat.checkSelfPermission(
+                context,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
     }
 }
 
-@Composable
-fun BluetoothDisabledScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.baseline_bluetooth_disabled_24),
-            contentDescription = "Bluetooth Disabled Logo",
-            modifier = Modifier.size(150.dp)
-        )
-        Text(text = "Votre Bluetooth n'est pas activé", modifier = Modifier.padding(16.dp))
-    }
-}
-
-@Composable
-fun BluetoothNotSupportedScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.baseline_bluetooth_disabled_24),
-            contentDescription = "Bluetooth Not Supported Logo",
-            modifier = Modifier.size(150.dp)
-        )
-        Text(
-            text = "Votre appareil ne supporte pas le Bluetooth",
-            modifier = Modifier.padding(16.dp)
-        )
-    }
-}
---
+++ app/src/main/java/com/example/androidsmartdevice/ScanAffichage.kt
@@ -0,0 +1,205 @@
+import android.annotation.SuppressLint
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.androidsmartdevice.R
+
+@Composable
+fun ScanActivityUI(
+    scanInteraction: ScanComposableInteraction
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Scan Activity", modifier = Modifier.padding(16.dp))
+        Image(
+            painter = painterResource(
+                id = if (scanInteraction.isSquareIcon.value) R.drawable.baseline_square_24 else R.drawable.baseline_play_arrow_24
+            ),
+            contentDescription = "Scan Icon",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(16.dp)
+                .clickable(onClick = scanInteraction::toggleButtonPlayScan)
+        )
+
+        Text(
+            text = if (scanInteraction.isScanning.value) "Scanning..." else "Click the icon to start scanning",
+            modifier = Modifier.padding(16.dp)
+        )
+        if (scanInteraction.isScanning.value) {
+            LinearProgressIndicator(modifier = Modifier.padding(16.dp))
+            DisplayDevices(scanInteraction.isScanning, scanInteraction.deviceResults.value)
+        }
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun DisplayBluetoothStatus(context: Context, status: String) {
+    val scanInteraction = ScanComposableInteraction(
+        mutableStateOf(false),
+        mutableStateOf(false),
+        mutableStateOf(emptyList()),
+        mutableStateOf(""),
+        {}
+    )
+    when (status) {
+        "notSupported" -> BluetoothNotSupportedScreen()
+        "disabled" -> BluetoothDisabledScreen()
+        "enabled" -> ScanActivityUI(scanInteraction)
+    }
+}
+
+// Display the list of connected devices
+@Composable
+fun DisplayDevices(isScanning: Boolean, devices: List<String>) {
+    if (isScanning) {
+        devices.forEach { device ->
+            Text(text = device, modifier = Modifier.padding(16.dp))
+        }
+    }
+}
+
+@Composable
+fun ScanActivityContent(scanInteraction: ScanComposableInteraction) {
+    ScanActivityUI(scanInteraction)
+}
+
+@Composable
+fun BluetoothDisabledScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_bluetooth_disabled_24),
+            contentDescription = "Bluetooth Disabled Logo",
+            modifier = Modifier.size(150.dp)
+        )
+        Text(text = "Votre Bluetooth n'est pas activé", modifier = Modifier.padding(16.dp))
+    }
+}
+
+@Composable
+fun BluetoothNotSupportedScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_bluetooth_disabled_24),
+            contentDescription = "Bluetooth Not Supported Logo",
+            modifier = Modifier.size(150.dp)
+        )
+        Text(
+            text = "Votre appareil ne supporte pas le Bluetooth",
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+
+data class Device(val name: String, val macAddress: String, val distance: Int)
+
+val fakeDevices = listOf(
+    Device("Device 1", "00:11:22:33:44:55", 10),
+    Device("Device 2", "66:77:88:99:AA:BB", 20),
+    Device("Device 3", "CC:DD:EE:FF:00:11", 30),
+    Device("Device 4", "22:33:44:55:66:77", 40),
+)
+
+class ScanComposableInteraction(
+    var isScanning: MutableState<Boolean>,
+    var isSquareIcon: MutableState<Boolean>,
+    var deviceResults: MutableState<List<Device>>, // Changed from List<String> to List<Device>
+    val hasBLEIssue: MutableState<String>,
+    val onIconClick: (ScanResult) -> Unit
+) {
+    fun toggleButtonPlayScan() {
+        isSquareIcon.value = !isSquareIcon.value
+        isScanning.value = !isScanning.value
+
+        // Si le scan est activé, affichez les appareils
+        if (isScanning.value) {
+            deviceResults.value = fakeDevices
+        } else {
+            deviceResults.value = emptyList()
+        }
+    }
+}
+@Composable
+fun DisplayDevices(isScanning: MutableState<Boolean>, devices: List<Device>) {
+    if (isScanning.value) {
+        LazyColumn {
+            items(devices) { device ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Text(text = device.name)
+                        Text(text = "MAC Address: ${device.macAddress}")
+                        DistanceIndicator(distance = device.distance)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DistanceIndicator(distance: Int) {
+    Box(
+        modifier = Modifier
+            .size(50.dp)
+            .clip(CircleShape)
+            .background(color = if (distance < 10) Color.Green else Color.Red),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$distance m",
+            color = Color.White,
+
+        )
+    }
+}
+
+
